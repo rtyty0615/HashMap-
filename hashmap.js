04f779b9bd7199b73b850aeeb4499376ed6502ff
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-04-14 21:53:06 +0800

    Add get(key)

--- hashmap.js
+++ hashmap.js
@@ -20,12 +20,15 @@ class HashMap {
         if (this.buckets[this.hash(key)] === null) {
             const list = new LinkedList();
             this.buckets[this.hash(key)] = list;
-        }
+        };
         this.buckets[this.hash(key)].append(key, value);
     }
 
     get(key) {
-
+        if (this.buckets[this.hash(key)] === null) {
+            return null
+        };
+        return this.buckets[this.hash(key)].findKey(key);
     }
 
     print() {
@@ -60,182 +63,22 @@ class LinkedList {
         current.nextNode = newNode
     }
 
-    prepend(value) {
-        const newNode = new Node(value);
-        if (this.head === null) {
-            this.head = newNode;
-            return
-        };
-        newNode.nextNode = this.head;
-        this.head = newNode
-    }
-
-    size() {
+    findKey(key) {
         let current = this.head;
-        let count = 0;
         if (current === null) {
-            return count
+            return null
         };
-        count += 1;
-        while (current.nextNode) {
-            current = current.nextNode;
-            count += 1;
-        };
-        return count
-    }
-
-    getHead() {
-        if (this.head === null) {
-            return undefined
-        };
-        return this.head.value
-    }
-
-    getTail() {
-        if (this.head === null) {
-            return undefined
-        };
-        let current = this.head;
-        while (current.nextNode) {
-            current = current.nextNode;
-        };
-        if (current.nextNode === null) {
+        if (current.key === key) {
             return current.value
         };
-    }
-
-    at(index) {
-        let current = this.head;
-        if (current === null) {
-            return undefined
-        };
-        if (index === 0) {
-            return current.value
-        };
-        for (let i = 0; i < index; i++) {
-            current = current.nextNode;
-            if (current === null) {
-                return undefined
-            };
-        }
-        return current.value;
-    }
-
-    pop() {
-        if (this.head === null) {
-            return undefined
-        };
-        const headValue = this.head.value;
-        this.head = this.head.nextNode;
-        return headValue
-    }
-
-    contains(input) {
-        let current = this.head;
-        if (current === null) {
-            return false
-        };
-        if (current.value === input) {
-            return true
-        };
         while (current.nextNode) {
             current = current.nextNode;
-            if (current.value === input) {
-                return true
+            if (current.key === key) {
+                return current.value
             }
         };
-        return false;
+        return null
     }
-
-    findIndex(input) {
-        let current = this.head;
-        let index = 0;
-        if (current === null) {
-            return -1
-        };
-        if (current.value === input) {
-            return index
-        };
-        while (current.nextNode) {
-            current = current.nextNode;
-            index += 1;
-            if (current.value === input) {
-                return index
-            }
-        };
-        return -1
-    }
-
-    toString() {
-        let listString = "";
-        let current = this.head;
-        if (current === null) {
-            return listString
-        };
-        listString += `(${current.value}) -> `;
-        while (current.nextNode) {
-            current = current.nextNode;
-            listString += `(${current.value}) -> `;
-        };
-        return listString += "null"
-    }
-
-    insertAt(index, ...inputs) {
-        let current = this.head;
-        let saveCurrent = current;
-        if (index < 0) {
-            return console.log(RangeError)
-        } else if (index > 0) {
-            for (let i = 0; i < index; i++) {
-                saveCurrent = current;
-                current = current.nextNode;
-                if (current === null) {
-                    return console.log(RangeError)
-                };
-            };
-
-            for (const input of inputs) {
-                const newNode = new Node(input);
-                saveCurrent.nextNode = newNode;
-                saveCurrent = saveCurrent.nextNode;
-            }
-            saveCurrent.nextNode = current
-        } else {
-            let count = 0;
-            for (const input of inputs) {
-                const newNode = new Node(input);
-                if (count === 0) {
-                    this.head = newNode; 
-                    saveCurrent = newNode;
-                    count += 1;
-                } else {
-                    saveCurrent.nextNode = newNode;
-                    saveCurrent = saveCurrent.nextNode;
-                }
-            }
-            saveCurrent.nextNode = current
-        };
-    }
-
-    removeAt(index) {
-        let current = this.head;
-        let saveCurrent = current;
-        if (index < 0) {
-            return console.log(RangeError)
-        } else if (index === 0) {
-            this.head = this.head.nextNode;
-        } else {
-            for (let i = 0; i < index; i++) {
-                saveCurrent = current;
-                current = current.nextNode;
-                if (current === null) {
-                    return console.log(RangeError)
-                };
-            };
-            saveCurrent.nextNode = current.nextNode;
-        }
-    }
-
 }
 
 const test = new HashMap();
@@ -255,4 +98,8 @@ test.set('lion', 'golden')
 
 test.print()
 
+test.get('apple')
+test.get('lion')
+test.get('hat')
+test.get('nian')
 
